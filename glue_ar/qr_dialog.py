5dc7ea426e1923ab847e8363f4b8f2e74c968973
Carifio24 <carifio24@gmail.com>
2024-01-08 01:59:35 -0600

    Create QR code inside of the dialog.

--- glue_ar/qr_dialog.py
+++ glue_ar/qr_dialog.py
@@ -2,19 +2,22 @@ from os.path import dirname
 
 from PIL.ImageQt import ImageQt
 from glue_qt.utils import load_ui
-from qtpy.QtWidgets import QDialog, QSizePolicy
+from qtpy.QtWidgets import QDialog
 from qtpy.QtGui import QPixmap
+
+from glue_ar.qr import create_qr
 
 
 class QRDialog(QDialog):
 
-    def __init__(self, parent=None, img=None):
+    def __init__(self, parent, url=None, img=None):
         
         super(QRDialog, self).__init__(parent=parent)
-
+        img = create_qr(url)
         self.img = ImageQt(img)
         self.pix = QPixmap.fromImage(self.img)
         self.ui = load_ui("qr_dialog.ui", self, directory=dirname(__file__))
+        self.ui.label_url.setText(f"<a href=\"{url}\">Open 3D view</a>")
         self.ui.label_image.setPixmap(self.pix)
         width, height = img.size
-        self.setFixedSize(width, height + 25)
+        self.setFixedSize(width, height + 70)
