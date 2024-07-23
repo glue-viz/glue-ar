c19c2533e2b5fea437051486c19a60405dc1717e
Carifio24 <carifio24@gmail.com>
2024-07-23 17:01:15 -0400

    Add some more testing of shape points.

--- glue_ar/common/tests/test_shapes.py
+++ glue_ar/common/tests/test_shapes.py
@@ -1,7 +1,7 @@
 from itertools import product
 from math import cos, sqrt
 from glue_ar.gltf_utils import index_maxes
-from ..shapes import rectangular_prism_points, rectangular_prism_triangulation, sphere_points
+from ..shapes import cone_points, cylinder_points, orthogonal_basis, rectangular_prism_points, rectangular_prism_triangulation, sphere_points
 
 from typing import Dict, Iterable, List, Tuple
 
@@ -59,33 +59,70 @@ class TestShapes:
                 (2.0, 1.5, 2.5)} == points
 
     def test_sphere_points(self):
-        center = (0, 0, 0)
-        radius = 1
+        center = (1, 2, 3)
+        radius = 2
         theta_resolution = 5
         phi_resolution = 8
 
-        points = set(sphere_points(
+        precision = 5
+
+        points = sphere_points(
                     center,
                     radius,
                     theta_resolution=theta_resolution,
                     phi_resolution=phi_resolution)
-                 )
+        points = {tuple(round(t, precision) for t in pt) for pt in points}
+
+        expected = {(1, 2, 5), (1, 2, 1)}
 
         half_sqrt2 = sqrt(2) / 2
-
-        expected =  set()
-        expected.add((0, 0, 1))
-        expected.add((0, 0, -1))
-
         xy = ((1, 0), (half_sqrt2, half_sqrt2), (0, 1), (-half_sqrt2, half_sqrt2),
               (-1, 0), (-half_sqrt2, -half_sqrt2), (0, -1), (half_sqrt2, -half_sqrt2))
-        z = (half_sqrt2, 0, -half_sqrt2)
-        sin_thetas = (half_sqrt2, 1, half_sqrt2)
-        zr = tuple(radius * s for s in sin_thetas)
+        z = (radius * half_sqrt2, 0, -radius * half_sqrt2)
+        zr = (radius * half_sqrt2, radius, radius * half_sqrt2)
 
         for vz, r in zip(z, zr):
             for (vx, vy) in xy:
-                expected.add((r * vx + center[0], r * vy + center[1], vz + center[2]))
+                point = (r * vx + 1, r * vy + 2, vz + 3)
+                expected.add(tuple(round(t, precision) for t in point))
+
+        assert points == expected
+
+    def test_cylinder_points(self):
+        center = (-1, 2, -5)
+        radius = 3
+        central_axis = (0, 0, 2)
+        length = 2
+        theta_resolution = 8
+
+        precision = 5
+        points = cylinder_points(center, radius, length, central_axis, theta_resolution)
+        points = {tuple(round(t, precision) for t in pt) for pt in points}
+
+
+        half_sqrt2 = sqrt(2) / 2
+        xy = ((1, 0), (half_sqrt2, half_sqrt2), (0, 1), (-half_sqrt2, half_sqrt2),
+              (-1, 0), (-half_sqrt2, -half_sqrt2), (0, -1), (half_sqrt2, -half_sqrt2))
+
+        expected = {(3 * vx - 1, 3 * vy + 2, vz) for (vx, vy), vz in product(xy, (-4, -6))}
+        expected = {tuple(round(t, precision) for t in pt) for pt in expected}
+
+        assert points == expected
+
+    def test_points(self):
+        center = (2, 0, -1)
+        radius = 6
+        height = 10
+        central_axis = (0, 1, 0)
+        theta_resolution = 4
+
+        precision = 5
+
+        points = set(cone_points(center, radius, height, central_axis, theta_resolution))
+        points = {tuple(round(t, precision) for t in pt) for pt in points}
+
+        expected = {(-4, 0, -7), (8, 0, 5), (-4, 0, 5), (8, 0, -7), (2, 10, -1)}
+        expected = {tuple(round(t, precision) for t in pt) for pt in expected}
 
         assert points == expected
 
