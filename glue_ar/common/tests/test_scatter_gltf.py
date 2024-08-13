7d33b2d21ad27ba71cef5c0ed3b7d6cc14fb309e
Carifio24 <carifio24@gmail.com>
2024-08-12 23:30:54 -0400

    Workaround for temporary file issue on Windows.

--- glue_ar/common/tests/test_scatter_gltf.py
+++ glue_ar/common/tests/test_scatter_gltf.py
@@ -1,3 +1,4 @@
+from os import remove
 from random import random, randint, seed
 from tempfile import NamedTemporaryFile
 
@@ -43,6 +44,9 @@ class TestScatterGLTF:
         }
 
     def teardown_method(self, method):
+        if getattr(self, "tmpfile", None) is not None:
+            self.tmpfile.close()
+            remove(self.tmpfile.name)
         self.viewer.close(warn=False)
         self.viewer = None
         self.app.close()
@@ -50,63 +54,63 @@ class TestScatterGLTF:
 
     def test_basic_export(self):
         bounds = xyz_bounds(self.viewer.state, with_resolution=False)
-        with NamedTemporaryFile(suffix=".gltf") as outfile:
-            export_viewer(self.viewer.state,
-                          [layer.state for layer in self.viewer.layers],
-                          bounds=bounds,
-                          state_dictionary=self.state_dictionary,
-                          filepath=outfile.name)
+        self.tmpfile = NamedTemporaryFile(suffix=".gltf", delete=False)
+        export_viewer(self.viewer.state,
+                      [layer.state for layer in self.viewer.layers],
+                      bounds=bounds,
+                      state_dictionary=self.state_dictionary,
+                      filepath=self.tmpfile.name)
 
-            gltf: GLTF = GLTF.load(outfile.name)
-            model = gltf.model
-            assert isinstance(model, GLTFModel)
+        gltf: GLTF = GLTF.load(self.tmpfile.name)
+        model = gltf.model
+        assert isinstance(model, GLTFModel)
 
-            assert model.buffers is not None and len(model.buffers) == 1
-            assert model.bufferViews is not None and len(model.bufferViews) == 1 + self.n
-            assert model.accessors is not None and len(model.accessors) == 1 + self.n
-            assert model.meshes is not None and len(model.meshes) == self.n
-            assert model.nodes is not None and len(model.nodes) == self.n
-            assert model.materials is not None and len(model.materials) == 1
+        assert model.buffers is not None and len(model.buffers) == 1
+        assert model.bufferViews is not None and len(model.bufferViews) == 1 + self.n
+        assert model.accessors is not None and len(model.accessors) == 1 + self.n
+        assert model.meshes is not None and len(model.meshes) == self.n
+        assert model.nodes is not None and len(model.nodes) == self.n
+        assert model.materials is not None and len(model.materials) == 1
 
-            material = model.materials[0]
-            layer = self.viewer.layers[0]
-            label = export_label_for_layer(layer)
-            method, options = self.state_dictionary[label]
-            assert method == "Scatter"
-            assert isinstance(options, ARScatterExportOptions)
-            color_components = [c / 256 for c in hex_to_components("#fedcba")] + [layer.state.alpha]
-            assert material.alphaMode == AlphaMode.BLEND.value
-            assert material.pbrMetallicRoughness is not None
-            assert material.pbrMetallicRoughness.baseColorFactor == color_components
-            assert material.pbrMetallicRoughness.roughnessFactor == 1
-            assert material.pbrMetallicRoughness.metallicFactor == 0
+        material = model.materials[0]
+        layer = self.viewer.layers[0]
+        label = export_label_for_layer(layer)
+        method, options = self.state_dictionary[label]
+        assert method == "Scatter"
+        assert isinstance(options, ARScatterExportOptions)
+        color_components = [c / 256 for c in hex_to_components("#fedcba")] + [layer.state.alpha]
+        assert material.alphaMode == AlphaMode.BLEND.value
+        assert material.pbrMetallicRoughness is not None
+        assert material.pbrMetallicRoughness.baseColorFactor == color_components
+        assert material.pbrMetallicRoughness.roughnessFactor == 1
+        assert material.pbrMetallicRoughness.metallicFactor == 0
 
-            assert all(mesh.primitives[0].indices == 0 for mesh in model.meshes)
+        assert all(mesh.primitives[0].indices == 0 for mesh in model.meshes)
 
-            theta_resolution: int = options.theta_resolution
-            phi_resolution: int = options.phi_resolution
-            triangles_count = sphere_triangles_count(theta_resolution=theta_resolution,
-                                                     phi_resolution=phi_resolution)
-            points_count = sphere_points_count(theta_resolution=theta_resolution,
-                                               phi_resolution=phi_resolution)
+        theta_resolution: int = options.theta_resolution
+        phi_resolution: int = options.phi_resolution
+        triangles_count = sphere_triangles_count(theta_resolution=theta_resolution,
+                                                 phi_resolution=phi_resolution)
+        points_count = sphere_points_count(theta_resolution=theta_resolution,
+                                           phi_resolution=phi_resolution)
 
-            assert count_indices(gltf, model.buffers[0], model.bufferViews[0]) == triangles_count
-            assert count_vertices(gltf, model.buffers[0], model.bufferViews[1]) == points_count
+        assert count_indices(gltf, model.buffers[0], model.bufferViews[0]) == triangles_count
+        assert count_vertices(gltf, model.buffers[0], model.bufferViews[1]) == points_count
 
-            assert model.bufferViews[0].target == BufferTarget.ELEMENT_ARRAY_BUFFER.value
-            assert model.bufferViews[1].target == BufferTarget.ARRAY_BUFFER.value
+        assert model.bufferViews[0].target == BufferTarget.ELEMENT_ARRAY_BUFFER.value
+        assert model.bufferViews[1].target == BufferTarget.ARRAY_BUFFER.value
 
-            indices_accessor = model.accessors[0]
-            assert indices_accessor.bufferView == 0
-            assert indices_accessor.componentType == ComponentType.UNSIGNED_INT.value
-            assert indices_accessor.count == triangles_count * 3
-            assert indices_accessor.type == AccessorType.SCALAR.value
-            assert indices_accessor.min == [0]
-            assert indices_accessor.max == [max(max(tri) for tri in sphere_triangles(theta_resolution=theta_resolution,
-                                                                                     phi_resolution=phi_resolution))]
+        indices_accessor = model.accessors[0]
+        assert indices_accessor.bufferView == 0
+        assert indices_accessor.componentType == ComponentType.UNSIGNED_INT.value
+        assert indices_accessor.count == triangles_count * 3
+        assert indices_accessor.type == AccessorType.SCALAR.value
+        assert indices_accessor.min == [0]
+        assert indices_accessor.max == [max(max(tri) for tri in sphere_triangles(theta_resolution=theta_resolution,
+                                                                                 phi_resolution=phi_resolution))]
 
-            for index, accessor in enumerate(model.accessors[1:]):
-                assert accessor.bufferView == index + 1
-                assert accessor.componentType == ComponentType.FLOAT.value
-                assert accessor.count == points_count
-                assert accessor.type == AccessorType.VEC3.value
+        for index, accessor in enumerate(model.accessors[1:]):
+            assert accessor.bufferView == index + 1
+            assert accessor.componentType == ComponentType.FLOAT.value
+            assert accessor.count == points_count
+            assert accessor.type == AccessorType.VEC3.value
--- glue_ar/common/tests/test_scatter_usd.py
+++ glue_ar/common/tests/test_scatter_usd.py
@@ -1,3 +1,4 @@
+from os import remove
 from random import random, randint, seed
 from tempfile import NamedTemporaryFile
 
@@ -42,6 +43,10 @@ class TestScatterUSD:
         }
 
     def teardown_method(self, method):
+        if getattr(self, "tmpfile", None) is not None:
+            self.tmpfile.close()
+            remove(self.tmpfile.name)
+
         self.viewer.close(warn=False)
         self.viewer = None
         self.app.close()
@@ -49,61 +54,61 @@ class TestScatterUSD:
 
     def test_basic_export(self):
         bounds = xyz_bounds(self.viewer.state, with_resolution=False)
-        with NamedTemporaryFile(suffix=".usdc") as outfile:
-            export_viewer(self.viewer.state,
-                          [layer.state for layer in self.viewer.layers],
-                          bounds=bounds,
-                          state_dictionary=self.state_dictionary,
-                          filepath=outfile.name)
+        self.tmpfile = NamedTemporaryFile(suffix=".usdc", delete=False)
+        export_viewer(self.viewer.state,
+                      [layer.state for layer in self.viewer.layers],
+                      bounds=bounds,
+                      state_dictionary=self.state_dictionary,
+                      filepath=self.tmpfile.name)
 
-            stage = Usd.Stage.Open(outfile.name)
-            world = stage.GetDefaultPrim()
-            assert str(world.GetPath()) == "/world"
+        stage = Usd.Stage.Open(self.tmpfile.name)
+        world = stage.GetDefaultPrim()
+        assert str(world.GetPath()) == "/world"
 
-            layer = self.viewer.layers[0]
-            label = export_label_for_layer(layer.state)
-            identifier = label.replace(" ", "_")
-            _, options = self.state_dictionary[label]
-            sphere_pts_count = sphere_points_count(theta_resolution=options.theta_resolution,
+        layer = self.viewer.layers[0]
+        label = export_label_for_layer(layer.state)
+        identifier = label.replace(" ", "_")
+        _, options = self.state_dictionary[label]
+        sphere_pts_count = sphere_points_count(theta_resolution=options.theta_resolution,
+                                               phi_resolution=options.phi_resolution)
+        sphere_tris_count = sphere_triangles_count(theta_resolution=options.theta_resolution,
                                                    phi_resolution=options.phi_resolution)
-            sphere_tris_count = sphere_triangles_count(theta_resolution=options.theta_resolution,
-                                                       phi_resolution=options.phi_resolution)
-            expected_vert_cts = [3] * sphere_tris_count
+        expected_vert_cts = [3] * sphere_tris_count
 
-            color_precision = 5
-            color_components = [round(c / 255, color_precision) for c in hex_to_components(layer.state.color)]
+        color_precision = 5
+        color_components = [round(c / 255, color_precision) for c in hex_to_components(layer.state.color)]
 
-            # There should be 2n + 4 total prims:
-            # The xform and the mesh for each point -> 2 * n
-            # The top-level world prim
-            # The light
-            # The material
-            # The PBR shader
-            assert iterator_count(stage.TraverseAll()) == 2 * self.n + 4
+        # There should be 2n + 4 total prims:
+        # The xform and the mesh for each point -> 2 * n
+        # The top-level world prim
+        # The light
+        # The material
+        # The PBR shader
+        assert iterator_count(stage.TraverseAll()) == 2 * self.n + 4
 
-            original_point_mesh = None
-            for i in range(self.n):
-                point_mesh = stage.GetPrimAtPath(f"/world/xform_{identifier}_{i}/mesh_{identifier}_{i}")
-                assert point_mesh is not None
-                points = list(point_mesh.GetAttribute("points").Get())
-                assert len(points) == sphere_pts_count
-                vertex_counts = list(point_mesh.GetAttribute("faceVertexCounts").Get())
-                assert vertex_counts == expected_vert_cts
-                vertex_indices = list(point_mesh.GetAttribute("faceVertexIndices").Get())
-                assert len(vertex_indices) == sphere_tris_count * 3
+        original_point_mesh = None
+        for i in range(self.n):
+            point_mesh = stage.GetPrimAtPath(f"/world/xform_{identifier}_{i}/mesh_{identifier}_{i}")
+            assert point_mesh is not None
+            points = list(point_mesh.GetAttribute("points").Get())
+            assert len(points) == sphere_pts_count
+            vertex_counts = list(point_mesh.GetAttribute("faceVertexCounts").Get())
+            assert vertex_counts == expected_vert_cts
+            vertex_indices = list(point_mesh.GetAttribute("faceVertexIndices").Get())
+            assert len(vertex_indices) == sphere_tris_count * 3
 
-                material = material_for_mesh(point_mesh)
-                pbr_shader = stage.GetPrimAtPath(f"{material.GetPath()}/PBRShader")
-                assert pbr_shader.GetAttribute("inputs:metallic").Get() == 0.0
-                assert pbr_shader.GetAttribute("inputs:roughness").Get() == 1.0
-                assert round(pbr_shader.GetAttribute("inputs:opacity").Get(), color_precision) == \
-                       round(layer.state.alpha, color_precision)
-                assert [round(c, color_precision) for c in pbr_shader.GetAttribute("inputs:diffuseColor").Get()] == \
-                       color_components
+            material = material_for_mesh(point_mesh)
+            pbr_shader = stage.GetPrimAtPath(f"{material.GetPath()}/PBRShader")
+            assert pbr_shader.GetAttribute("inputs:metallic").Get() == 0.0
+            assert pbr_shader.GetAttribute("inputs:roughness").Get() == 1.0
+            assert round(pbr_shader.GetAttribute("inputs:opacity").Get(), color_precision) == \
+                   round(layer.state.alpha, color_precision)
+            assert [round(c, color_precision) for c in pbr_shader.GetAttribute("inputs:diffuseColor").Get()] == \
+                   color_components
 
-                if i == 0:
-                    original_point_mesh = point_mesh
-                else:
-                    stack_top = point_mesh.GetPrimStack()[0]
-                    assert stack_top.referenceList.prependedItems[0] == \
-                           Sdf.Reference(primPath=original_point_mesh.GetPath())
+            if i == 0:
+                original_point_mesh = point_mesh
+            else:
+                stack_top = point_mesh.GetPrimStack()[0]
+                assert stack_top.referenceList.prependedItems[0] == \
+                       Sdf.Reference(primPath=original_point_mesh.GetPath())
