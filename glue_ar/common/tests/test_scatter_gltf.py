bc59e8c679cab0c57fedced7167d3a4bbf402ea8
Carifio24 <carifio24@gmail.com>
2024-09-03 01:30:07 -0400

    Update scatter glTF tests.

--- glue_ar/common/tests/test_scatter_gltf.py
+++ glue_ar/common/tests/test_scatter_gltf.py
@@ -1,33 +1,44 @@
 from os import remove
 from random import random, randint, seed
 from tempfile import NamedTemporaryFile
+from typing import Dict, Tuple, Type, Union
 
 from gltflib import AccessorType, AlphaMode, BufferTarget, ComponentType, GLTFModel
 from gltflib.gltf import GLTF
 from glue.core import Data
+from glue.core.state_objects import State
+from glue.viewers.common.viewer import Viewer
+from glue_jupyter import JupyterApplication
+from glue_jupyter.ipyvolume.scatter import IpyvolumeScatterView
 from glue_qt.app import GlueApplication
 from glue_vispy_viewers.scatter.qt.scatter_viewer import VispyScatterViewer
+import pytest
 
 from glue_ar.common.export import export_viewer
-from glue_ar.common.scatter_export_options import ARVispyScatterExportOptions
+from glue_ar.common.scatter_export_options import ARIpyvolumeScatterExportOptions, ARVispyScatterExportOptions
 from glue_ar.common.shapes import sphere_points_count, sphere_triangles, sphere_triangles_count
 from glue_ar.common.tests.gltf_helpers import count_indices, count_vertices, unpack_vertices
-from glue_ar.utils import export_label_for_layer, hex_to_components, mask_for_bounds, xyz_bounds, xyz_for_layer
+from glue_ar.utils import export_label_for_layer, hex_to_components, layers_to_export, mask_for_bounds, xyz_bounds, xyz_for_layer
 
 
+@pytest.mark.parametrize("app_type,viewer_type", (("qt", "vispy"), ("jupyter", "vispy"), ("jupyter", "ipyvolume")))
 class TestScatterGLTF:
 
-    def setup_method(self, method):
+    @pytest.fixture(scope='function', autouse=True)
+    def setup_and_teardown(self, app_type: str, viewer_type: str):
+
+        # Setup
         seed(1374)
-        self.n = 40
+        self.n = 80
         x1 = [random() * 5 for _ in range(self.n)]
         y1 = [random() for _ in range(self.n)]
         z1 = [randint(1, 30) for _ in range(self.n)]
         self.data1 = Data(x=x1, y=y1, z=z1, label="Scatter Data 1")
         self.data1.style.color = "#fedcba"
-        self.app = GlueApplication()
+        self.app = self._create_application(app_type)
         self.app.data_collection.append(self.data1)
-        self.viewer = self.app.new_data_viewer(VispyScatterViewer)
+        self.viewer_type = viewer_type
+        self.viewer: Viewer = self.app.new_data_viewer(self._viewer_class(viewer_type))
         self.viewer.add_data(self.data1)
 
         x2 = [random() * 7 for _ in range(self.n)]
@@ -38,29 +49,64 @@ class TestScatterGLTF:
         self.viewer.state.x_att = self.data1.id['x']
         self.viewer.state.y_att = self.data1.id['y']
         self.viewer.state.z_att = self.data1.id['z']
-        self.state_dictionary = {
-            export_label_for_layer(layer): ("Scatter", ARVispyScatterExportOptions())
-            for layer in self.viewer.layers
-        }
+        self.state_dictionary = self._basic_state_dictionary(viewer_type)
 
-    def teardown_method(self, method):
+        yield
+
+        # Teardown
         if getattr(self, "tmpfile", None) is not None:
             self.tmpfile.close()
             remove(self.tmpfile.name)
-        self.viewer.close(warn=False)
+        if hasattr(self.viewer, "close"):
+            self.viewer.close(warn=False)
         self.viewer = None
-        self.app.close()
+        if hasattr(self.app, 'close'):
+            self.app.close()
         self.app = None
 
+    def _create_application(self, app_type: str) -> Union[GlueApplication, JupyterApplication]:
+        if app_type == "qt":
+            return GlueApplication()
+        elif app_type == "jupyter":
+            return JupyterApplication()
+        else:
+            raise ValueError("Application type should be either qt or jupyter")
+
+    def _viewer_class(self, viewer_type: str) -> Union[Type[VispyScatterViewer], Type[IpyvolumeScatterView]]:
+        if viewer_type == "vispy":
+            return VispyScatterViewer
+        elif viewer_type == "ipyvolume":
+            return IpyvolumeScatterView
+        else:
+            raise ValueError("Viewer type should be either vispy or ipyvolume")
+
+    def _basic_state_dictionary(self, viewer_type: str) -> Dict[str, Tuple[str, State]]:
+        if viewer_type == "vispy":
+            state_maker = lambda: ARVispyScatterExportOptions(theta_resolution=15,
+                                                              phi_resolution=15)
+        elif viewer_type == "ipyvolume":
+            state_maker = ARIpyvolumeScatterExportOptions
+        else:
+            raise ValueError("Viewer type should be either vispy or ipyvolume")
+
+        return {
+            export_label_for_layer(layer): ("Scatter", state_maker())
+            for layer in self.viewer.layers
+        }
+
+
+    # TODO: How can we test the properties of compressed files?
     def test_basic_export(self):
         bounds = xyz_bounds(self.viewer.state, with_resolution=False)
         self.tmpfile = NamedTemporaryFile(suffix=".gltf", delete=False)
         self.tmpfile.close()
+        layer_states = [layer.state for layer in layers_to_export(self.viewer)]
         export_viewer(self.viewer.state,
-                      [layer.state for layer in self.viewer.layers],
+                      layer_states=layer_states,
                       bounds=bounds,
                       state_dictionary=self.state_dictionary,
-                      filepath=self.tmpfile.name)
+                      filepath=self.tmpfile.name,
+                      compression=None)
 
         gltf: GLTF = GLTF.load(self.tmpfile.name)
         model = gltf.model
@@ -78,7 +124,8 @@ class TestScatterGLTF:
         label = export_label_for_layer(layer)
         method, options = self.state_dictionary[label]
         assert method == "Scatter"
-        assert isinstance(options, ARVispyScatterExportOptions)
+        export_state_cls = ARIpyvolumeScatterExportOptions if self.viewer_type == "ipyvolume" else ARVispyScatterExportOptions
+        assert isinstance(options, export_state_cls)
         color_components = [c / 256 for c in hex_to_components("#fedcba")] + [layer.state.alpha]
         assert material.alphaMode == AlphaMode.BLEND.value
         assert material.pbrMetallicRoughness is not None
@@ -88,8 +135,10 @@ class TestScatterGLTF:
 
         assert all(mesh.primitives[0].indices == 0 for mesh in model.meshes)
 
-        theta_resolution: int = options.theta_resolution
-        phi_resolution: int = options.phi_resolution
+        # TODO: 3 is the value for ipyvolume's diamond, which is the ipv default
+        # But we should make this more robust
+        theta_resolution: int = getattr(options, "theta_resolution", 3)
+        phi_resolution: int = getattr(options, "phi_resolution", 3)
         triangles_count = sphere_triangles_count(theta_resolution=theta_resolution,
                                                  phi_resolution=phi_resolution)
         points_count = sphere_points_count(theta_resolution=theta_resolution,
