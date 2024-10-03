1f9a0c100103b04a26c2f345939f94961c2ff524
Carifio24 <carifio24@gmail.com>
2024-10-03 12:45:18 -0400

    Update resolution calculation to account for scatter layers in ipyvolume viewers.

--- glue_ar/utils.py
+++ glue_ar/utils.py
@@ -279,7 +279,8 @@ def get_resolution(viewer_state: Viewer3DState) -> int:
     try:
         from glue_jupyter.common.state3d import VolumeViewerState
         if isinstance(viewer_state, VolumeViewerState):
-            return max((getattr(state, 'max_resolution', 256) for state in viewer_state.layers), default=256)
+            resolutions = tuple(getattr(state, 'max_resolution', None) for state in viewer_state.layers)
+            return max((res for res in resolutions if res is not None), default=256)
     except ImportError:
         pass
 
