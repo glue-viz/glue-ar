f87ac61a05fa4f2badcbc99bdd7e4af8c9954070
Carifio24 <carifio24@gmail.com>
2024-10-29 11:04:24 -0400

    Start working on points export for USD as well.

---
+++ glue_ar/common/points_usd.py
@@ -0,0 +1,89 @@
+from collections import defaultdict
+from gltflib import AccessorType, BufferTarget, ComponentType, PrimitiveMode
+from glue_vispy_viewers.common.viewer_state import Vispy3DViewerState
+from glue_vispy_viewers.scatter.layer_state import ScatterLayerState
+
+from glue_ar.common.export_options import ar_layer_export
+from glue_ar.common.scatter import Scatter3DLayerState, ScatterLayerState3D, scatter_layer_mask
+from glue_ar.usd_utils import material_for_color
+from glue_ar.utils import Bounds, NoneType, Viewer3DState, color_identifier, hex_to_components, layer_color, unique_id, xyz_bounds, xyz_for_layer
+from glue_ar.common.usd_builder import USDBuilder
+from glue_ar.common.scatter_export_options import ARPointExportOptions
+
+try:
+    from glue_jupyter.common.state3d import ViewerState3D
+except ImportError:
+    ViewerState3D = NoneType
+
+
+def add_points_layer_usd(builder: USDBuilder,
+                         viewer_state: Viewer3DState,
+                         layer_state: ScatterLayerState3D,
+                         bounds: Bounds,
+                         clip_to_bounds: bool = True):
+
+    if layer_state is None:
+        return
+
+    bounds = xyz_bounds(viewer_state, with_resolution=False)
+
+    vispy_layer_state = isinstance(layer_state, ScatterLayerState)
+    color_mode_attr = "color_mode" if vispy_layer_state else "cmap_mode"
+    fixed_color = getattr(layer_state, color_mode_attr, "Fixed") == "Fixed"
+
+    mask = scatter_layer_mask(viewer_state, layer_state, bounds, clip_to_bounds)
+    data = xyz_for_layer(viewer_state, layer_state,
+                         preserve_aspect=viewer_state.native_aspect,
+                         mask=mask,
+                         scaled=True)
+    data = data[:, [1, 2, 0]]
+
+    identifier = f"layer_{unique_id()}"
+
+    if fixed_color:
+        color = layer_color(layer_state)
+        components = hex_to_components(color)[:3]
+        colors = [components for _ in range(data.shape[0])]
+    else:
+        cmap = layer_state.cmap
+        cmap_attr = "cmap_attribute" if vispy_layer_state else "cmap_att"
+        cmap_att = getattr(layer_state, cmap_attr)
+        cmap_vals = layer_state.layer[cmap_att][mask]
+        crange = layer_state.cmap_vmax - layer_state.cmap_vmin
+
+        def get_color(cval):
+            normalized = max(min((cval - layer_state.cmap_vmin) / crange, 1), 0)
+            cindex = int(normalized * 255)
+            return cmap(cindex)[:3]
+
+        colors = [get_color(cval) for cval in cmap_vals]
+
+    builder.add_points(data, colors, identifier)
+   
+
+@ar_layer_export(ScatterLayerState, "Points", ARPointExportOptions, ("usdz", "usdc", "usda"))
+def add_vispy_points_layer_usd(builder: USDBuilder,
+                               viewer_state: Vispy3DViewerState,
+                               layer_state: ScatterLayerState,
+                               options: ARPointExportOptions,
+                               bounds: Bounds,
+                               clip_to_bounds: bool = True):
+    add_points_layer_usd(builder=builder,
+                         viewer_state=viewer_state,
+                         layer_state=layer_state,
+                         bounds=bounds,
+                         clip_to_bounds=clip_to_bounds)
+
+
+@ar_layer_export(Scatter3DLayerState, "Points", ARPointExportOptions, ("usdz", "usdc", "usda"))
+def add_ipyvolume_points_layer_usd(builder: USDBuilder,
+                                   viewer_state: ViewerState3D,
+                                   layer_state: ScatterLayerState,
+                                   options: ARPointExportOptions,
+                                   bounds: Bounds,
+                                   clip_to_bounds: bool = True):
+    add_points_layer_usd(builder=builder,
+                         viewer_state=viewer_state,
+                         layer_state=layer_state,
+                         bounds=bounds,
+                         clip_to_bounds=clip_to_bounds)
--- glue_ar/common/usd_builder.py
+++ glue_ar/common/usd_builder.py
@@ -2,8 +2,9 @@ from collections import defaultdict
 from os import extsep, remove
 from os.path import splitext
 
-from pxr import Usd, UsdGeom, UsdLux, UsdShade, UsdUtils
+from pxr import Gf, Sdf, Usd, UsdGeom, UsdLux, UsdShade, UsdUtils
 from typing import Dict, Iterable, Optional, Tuple
+from glue_ar.common.scatter import Point
 
 from glue_ar.usd_utils import material_for_color, material_for_mesh
 from glue_ar.utils import unique_id
@@ -37,7 +38,7 @@ class USDBuilder:
         self.default_prim = UsdGeom.Xform.Define(self.stage, self.default_prim_key).GetPrim()
         self.stage.SetDefaultPrim(self.default_prim)
 
-        self._mesh_counts: Dict[str, int] = defaultdict(int)
+        self._geom_counts: Dict[str, int] = defaultdict(int)
 
         light = UsdLux.RectLight.Define(self.stage, "/light")
         light.CreateHeightAttr(-1)
@@ -76,11 +77,11 @@ class USDBuilder:
         """
         identifier = identifier or unique_id()
         identifier = self._sanitize(identifier)
-        count = self._mesh_counts[identifier]
+        count = self._geom_counts[identifier]
         xform_key = f"{self.default_prim_key}/xform_{identifier}_{count}"
         UsdGeom.Xform.Define(self.stage, xform_key)
         mesh_key = f"{xform_key}/mesh_{identifier}_{count}"
-        self._mesh_counts[identifier] += 1
+        self._geom_counts[identifier] += 1
         mesh = UsdGeom.Mesh.Define(self.stage, mesh_key)
         mesh.CreateSubdivisionSchemeAttr().Set(UsdGeom.Tokens.none)
         mesh.CreatePointsAttr(points)
@@ -101,11 +102,11 @@ class USDBuilder:
         prim = mesh.GetPrim()
         identifier = identifier or unique_id()
         identifier = self._sanitize(identifier)
-        count = self._mesh_counts[identifier]
+        count = self._geom_counts[identifier]
         xform_key = f"{self.default_prim_key}/xform_{identifier}_{count}"
         UsdGeom.Xform.Define(self.stage, xform_key)
         new_mesh_key = f"{xform_key}/mesh_{identifier}_{count}"
-        self._mesh_counts[identifier] += 1
+        self._geom_counts[identifier] += 1
         new_mesh = UsdGeom.Mesh.Define(self.stage, new_mesh_key)
         new_prim = new_mesh.GetPrim()
 
@@ -121,6 +122,30 @@ class USDBuilder:
         translate_op.Set(value=translation)
 
         return mesh
+
+    def add_points(self,
+                   points: Iterable[Point],
+                   colors: Iterable[Tuple[int, int, int]],
+                   identifier: Optional[str] = None) -> UsdGeom.Points:
+
+        identifier = identifier or unique_id()
+        identifier = self._sanitize(identifier)
+        count = self._geom_counts[identifier]
+        xform_key = f"{self.default_prim_key}/xform_{identifier}_{count}"
+        UsdGeom.Xform.Define(self.stage, xform_key)
+        points_key = f"{xform_key}/points_{identifier}_{count}"
+        self._geom_counts[identifier] += 1
+        geom = UsdGeom.Points.Define(self.stage, points_key)
+        point_vecs = [Gf.Vec3f(*point) for point in points]
+        geom.CreatePointsAttr(point_vecs)
+        widths_var = geom.CreateWidthsAttr()
+        widths = [1.0 for _ in points]
+        widths_var.Set(widths)
+        # primvars = UsdGeom.PrimvarsAPI(geom.GetPrim())
+        # colorvar = primvars.CreatePrimvar("displayColor", Sdf.ValueTypeNames.Color3f)
+        # colorvar.Set([Gf.Vec3f(*color) for color in colors])
+
+        return geom
 
     def export(self, filepath: str):
         base, ext = splitext(filepath)
