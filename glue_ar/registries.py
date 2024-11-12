e3cd9a16b30e779e0385cf8e5e886a3670bde59b
Carifio24 <carifio24@gmail.com>
2024-11-12 16:53:20 -0500

    Use registries for builders and compressors rather than hardcoded dictionaries.

---
+++ glue_ar/registries.py
@@ -0,0 +1,39 @@
+from collections.abc import Callable
+from typing import Tuple, Type
+
+from glue.config import DictRegistry
+
+
+__all__ = ["builder", "compressor"]
+
+
+class BuilderRegistry(DictRegistry):
+
+    def add(self, extensions: str | Tuple[str], builder: Type):
+        if isinstance(extensions, str):
+            self._members[extensions] = builder
+        else:
+            for ext in extensions:
+                self._members[ext] = builder
+
+    def __call__(self, extensions: str | Tuple[str]):
+        def adder(builder: Type):
+            self.add(extensions, builder)
+        return adder 
+
+
+builder = BuilderRegistry()
+
+
+class CompressorRegistry(DictRegistry):
+
+    def add(self, name: str, compressor: Callable[[str], None]):
+        self._members[name] = compressor
+
+    def __call__(self, name: str):
+        def adder(compressor: Callable[[str], None]):
+            self.add(name, compressor)
+        return adder
+
+
+compressor = CompressorRegistry()
