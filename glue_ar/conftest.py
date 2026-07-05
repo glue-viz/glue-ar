767ead123c1c285a5a3789de42e7395745b9da79
Carifio24 <carifio24@gmail.com>
2026-07-05 02:10:46 -0400

    Bump version of glue-core and updating utilities tests to use actual 3D data.

--- glue_ar/conftest.py
+++ glue_ar/conftest.py
@@ -1,3 +1,21 @@
+import pytest
+
+from glue.core import Data
+import numpy as np
+
+
 def pytest_configure(config):
     from glue_ar import setup
     setup()
+
+
+@pytest.fixture
+def data_3d():
+    x = np.arange(24).reshape((2, 3, 4))
+    return Data(label="Data", x=x)
+
+
+@pytest.fixture
+def data2_3d():
+    x = np.arange(36).reshape((3, 4, 3))
+    return Data(label="Data", x=x)
